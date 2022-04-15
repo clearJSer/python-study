12d127d8ac9e161bff3bb5cebc36e08384e86fb6
lakiliu <lakiliu@tencent.com>
2022-04-15 16:36:07 +0800

    python 练习

---
+++ format.py
@@ -0,0 +1,5 @@
+
+str = '{0},{1}'.format('kfc', 18)
+
+str = '{age},{name}'.format(age = 10, name='123')
+print(str)
---
+++ 日期处理.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+# 获取今天的时间
+time = datetime.today()
+
+print(time)
