5b08fd2c40187b60580ea831cf420d3cc4b269a0
lakiliu <lakiliu@tencent.com>
2022-05-24 21:37:16 +0800

    update

---
+++ 10-暂停1s输出格式化时间.py
@@ -0,0 +1,9 @@
+import time
+
+myD = {1: 'a', 2: 'asdfas'}
+
+print(time.strftime('%Y-%m-%d %H:%m:%S'))
+
+time.sleep(3)
+
+print(time.strftime('%Y-%m-%d %H:%m:%S'))
---
+++ 9-暂停1s输出.py
@@ -0,0 +1,7 @@
+import time
+
+myD = {1: 'a', 2: 'asdfas'}
+
+for key,values in dict.items(myD):
+    print(key, values)
+    time.sleep(3) # 暂停3s
